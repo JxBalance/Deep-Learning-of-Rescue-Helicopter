3206d850d7d69bcbafc139c658aac8396d9c2f7f
Jixiang <34788672+JxBalance@users.noreply.github.com>
2018-12-27 11:07:36 +0800

    Add files via upload

---
+++ Testing Environment (Dynamic)/Assets/AgedRiverBoat/Readme.txt
@@ -0,0 +1,14 @@
+Aged River Boat version 1.01
+Autor: metadesc, Helmut Kleber
+Website: www.metadesc.com
+
+
+Required Unity standard assets for the scene:
+
+- Effects
+- Environment
+
+Unzip the AgedRiverBoat.zip file, if you need the Blender file. Use the Substance Painter (1.73)
+for changing the textures.
+
+Enjoy the boat!
---
+++ Testing Environment (Dynamic)/Assets/AgedRiverBoat/Scripts/BouyancySimple.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouyancySimple : MonoBehaviour {
+
+	public float frequencyMin = 1.0f;
+	public float frequencyMax = 2.0f;
+	public float magnitude = 0.0025f;
+	private float randomInterval;
+	
+	void Start () {
+		randomInterval = Random.Range(frequencyMin, frequencyMax);
+	}
+	
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.y += (Mathf.Cos(Time.time * randomInterval) * magnitude);
+		transform.position = pos;
+
+		//Vector3 angle = transform.eulerAngles;
+		//angle.y += (Mathf.Cos(Time.time * randomInterval) * 2);
+		//transform.eulerAngles = angle;
+	}
+
+}
